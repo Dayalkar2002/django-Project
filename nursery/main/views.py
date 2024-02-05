e22615171488fa9af4de10d14793203ae343a6ee
Dayalkar2002 <dayalkarmanish123@gmail.com>
2024-02-05 11:18:03 +0530

    update

--- nursery/main/views.py
+++ nursery/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'main/home.html')
+    return render(request,'main/home.html',)
 
 def signin(request):
     return render(request,'main/signin.html')
